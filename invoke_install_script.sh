08157b0714a6b550438feb426343489ddc48fa90
Zav <zav@cherre.com>
2019-11-01 16:34:09 -0400

    CODESYNC Fri Nov  1 16:34:09 EDT 2019 [skip ci]

--- invoke_install_script.sh
+++ invoke_install_script.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-cur_path="$(dirname ${BASH_SOURCE[0]})"
-echo "Running @ $cur_path"
 echo "$PWD"
-"$cur_path/src/install" "$@" || exit $?
+ls -la
+"$PWD/src/install" "$@" || exit $?

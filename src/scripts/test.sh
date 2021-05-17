ccfa818baf1b7a9295dc09914b71740fad0717f5
zav shotan <zav.shotan@gmail.com>
2021-05-16 20:02:43 -0400

    added tests

--- src/scripts/test.sh
+++ src/scripts/test.sh
@@ -9,17 +9,23 @@ function assert() {
   return $code
 }
 
-function test_method_call() {
+function zbash_config_test_counts() {
   local counts="$ZBASH_CONFIG_TEST_CYCLE_COUNT"
   : "${counts:="100"}"
+  echo "$counts"
+}
 
-  for i in $(seq 1 $counts); do
+function zbash_config_test_method_call() {
+  for i in $(seq 1 "$(zbash_config_test_counts)"); do
     "$@" >>/dev/null
     assert $? " When invoking: " "$@" || return $?
   done
 }
 
 function zbash_config_test_git_speed() {
-  test_method_call
-  zbash_config_fast_git
+  local counts="$(zbash_config_test_counts)"
+  echo "Checking $counts times get git branch"
+  time zbash_config_test_method_call prompt_git || return $?
+  echo "Checking $counts times get git status"
+  time zbash_config_test_method_call prompt_git_status || return $?
 }
